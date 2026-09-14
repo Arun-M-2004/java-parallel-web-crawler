48b574b453f6fd60c4a1924919a9c93dbe06f58a
Arun-M-2004 <152045500+Arun-M-2004@users.noreply.github.com>
2026-09-14 12:48:34 +0530

    Add Java parallel web crawler

---
+++ starter/webcrawler/src/main/java/com/udacity/webcrawler/json/CrawlerConfiguration.java
@@ -0,0 +1,263 @@
+package com.udacity.webcrawler.json;
+
+import java.time.Duration;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/**
+ * A data class that represents the configuration of a single web crawl.
+ */
+@JsonDeserialize(builder = CrawlerConfiguration.Builder.class)
+public final class CrawlerConfiguration {
+
+  private final List<String> startPages;
+  private final List<Pattern> ignoredUrls;
+  private final List<Pattern> ignoredWords;
+  private final int parallelism;
+  private final String implementationOverride;
+  private final int maxDepth;
+  private final Duration timeout;
+  private final int popularWordCount;
+  private final String profileOutputPath;
+  private final String resultPath;
+
+  private CrawlerConfiguration(
+          List<String> startPages,
+          List<Pattern> ignoredUrls,
+          List<Pattern> ignoredWords,
+          int parallelism,
+          String implementationOverride,
+          int maxDepth,
+          Duration timeout,
+          int popularWordCount,
+          String profileOutputPath,
+          String resultPath) {
+    this.startPages = startPages;
+    this.ignoredUrls = ignoredUrls;
+    this.ignoredWords = ignoredWords;
+    this.parallelism = parallelism;
+    this.implementationOverride = implementationOverride;
+    this.maxDepth = maxDepth;
+    this.timeout = timeout;
+    this.popularWordCount = popularWordCount;
+    this.profileOutputPath = profileOutputPath;
+    this.resultPath = resultPath;
+  }
+
+  /**
+   * An unmodifiable {@link List} of URLs that define the starting points of the web crawl. It
+   * should not contain any duplicate URLs.
+   */
+  public List<String> getStartPages() {
+    return startPages;
+  }
+
+  /**
+   * A {@link List} of regular expression {@link Pattern}s that determine which URLs, if any, the
+   * web crawler should not follow.
+   */
+  public List<Pattern> getIgnoredUrls() {
+    return ignoredUrls;
+  }
+
+  /**
+   * A {@link List} of regular expression {@link Pattern}s that determine which words, if any, the
+   * web crawler should not be counted toward the popular word count returned from the crawl.
+   */
+  public List<Pattern> getIgnoredWords() {
+    return ignoredWords;
+  }
+
+  /**
+   * The desired parallelism (e.g., number of CPU cores) that should be used for the web crawl.
+   */
+  public int getParallelism() {
+    return parallelism;
+  }
+
+  /**
+   * An explicit override for which web crawler implementation should be used for this crawl.
+   */
+  public String getImplementationOverride() {
+    return implementationOverride;
+  }
+
+  /**
+   * The maximum depth allowed for the crawl.
+   */
+  public int getMaxDepth() {
+    return maxDepth;
+  }
+
+  /**
+   * The maximum amount of time the crawler is allowed to run.
+   */
+  public Duration getTimeout() {
+    return timeout;
+  }
+
+  /**
+   * The number of popular words the crawler should record in its output.
+   */
+  public int getPopularWordCount() {
+    return popularWordCount;
+  }
+
+  /**
+   * Path to the output file where performance data from this web crawl should be written.
+   */
+  public String getProfileOutputPath() {
+    return profileOutputPath;
+  }
+
+  /**
+   * Path to the output file where the result data from this web crawl should be written.
+   */
+  public String getResultPath() {
+    return resultPath;
+  }
+
+  /**
+   * A builder class to create {@link CrawlerConfiguration} instances.
+   */
+  public static final class Builder {
+    private final Set<String> startPages = new LinkedHashSet<>();
+    private final Set<String> ignoredUrls = new LinkedHashSet<>();
+    private final Set<String> ignoredWords = new LinkedHashSet<>();
+    private int parallelism = -1;
+    private String implementationOverride = "";
+    private int maxDepth = 0;
+    private int timeoutSeconds = 1;
+    private int popularWordCount = 0;
+    private String profileOutputPath = "";
+    private String resultPath = "";
+
+    /**
+     * Adds a start page URL.
+     */
+    @JsonProperty("startPages")
+    public Builder addStartPages(String... startPages) {
+      for (String startPage : startPages) {
+        this.startPages.add(Objects.requireNonNull(startPage));
+      }
+      return this;
+    }
+
+    /**
+     * Adds a regular expression pattern that defines URLs to ignore during the crawl.
+     */
+    @JsonProperty("ignoredUrls")
+    public Builder addIgnoredUrls(String... patterns) {
+      for (String pattern : patterns) {
+        ignoredUrls.add(Objects.requireNonNull(pattern));
+      }
+      return this;
+    }
+
+    /**
+     * Adds a regular expression pattern that defines words to ignore.
+     */
+    @JsonProperty("ignoredWords")
+    public Builder addIgnoredWords(String... patterns) {
+      for (String pattern : patterns) {
+        ignoredWords.add(Objects.requireNonNull(pattern));
+      }
+      return this;
+    }
+
+    /**
+     * Sets the desired parallelism of the crawl.
+     */
+    @JsonProperty("parallelism")
+    public Builder setParallelism(int parallelism) {
+      this.parallelism = parallelism;
+      return this;
+    }
+
+    /**
+     * Overrides the {@link com.udacity.webcrawler.WebCrawler} implementation.
+     */
+    @JsonProperty("implementationOverride")
+    public Builder setImplementationOverride(String implementationOverride) {
+      this.implementationOverride = Objects.requireNonNull(implementationOverride);
+      return this;
+    }
+
+    /**
+     * Sets the maximum depth of the crawl.
+     */
+    @JsonProperty("maxDepth")
+    public Builder setMaxDepth(int maxDepth) {
+      this.maxDepth = maxDepth;
+      return this;
+    }
+
+    /**
+     * Sets the maximum amount of time allowed for the crawl, specified in seconds.
+     */
+    @JsonProperty("timeoutSeconds")
+    public Builder setTimeoutSeconds(int seconds) {
+      this.timeoutSeconds = seconds;
+      return this;
+    }
+
+    /**
+     * Sets the number of most popular words that should be reported by the crawl.
+     */
+    @JsonProperty("popularWordCount")
+    public Builder setPopularWordCount(int popularWordCount) {
+      this.popularWordCount = popularWordCount;
+      return this;
+    }
+
+    /**
+     * Sets the path to the file where profiling data for this crawl should be written.
+     */
+    @JsonProperty("profileOutputPath")
+    public Builder setProfileOutputPath(String profileOutputPath) {
+      this.profileOutputPath = Objects.requireNonNull(profileOutputPath);
+      return this;
+    }
+
+    /**
+     * Sets the path to the file where the result of this crawl should be written.
+     */
+    @JsonProperty("resultPath")
+    public Builder setResultPath(String resultPath) {
+      this.resultPath = Objects.requireNonNull(resultPath);
+      return this;
+    }
+
+    /**
+     * Constructs a {@link CrawlerConfiguration} from this builder.
+     */
+    public CrawlerConfiguration build() {
+      if (maxDepth < 0) {
+        throw new IllegalArgumentException("maxDepth cannot be negative");
+      }
+      if (timeoutSeconds <= 0) {
+        throw new IllegalArgumentException("timeoutSeconds must be positive");
+      }
+      if (popularWordCount < 0) {
+        throw new IllegalArgumentException("popularWordCount cannot be negative");
+      }
+      return new CrawlerConfiguration(
+              startPages.stream().collect(Collectors.toUnmodifiableList()),
+              ignoredUrls.stream().map(Pattern::compile).collect(Collectors.toUnmodifiableList()),
+              ignoredWords.stream().map(Pattern::compile).collect(Collectors.toUnmodifiableList()),
+              parallelism,
+              implementationOverride,
+              maxDepth,
+              Duration.ofSeconds(timeoutSeconds),
+              popularWordCount,
+              profileOutputPath,
+              resultPath);
+    }
+  }
+}
